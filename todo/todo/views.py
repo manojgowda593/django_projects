9663fb2dced1b75f276d1f4b97fae9744a0399bf
Manoj Gowda <manoj.gowda1593@gmail.com>
2025-07-21 17:04:59 +0530

    SignUp and Login page

---
+++ todo/todo/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from .form import Signup
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+
+
+def signup(request):
+    if request.method=='POST':
+        form = Signup(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request,'signup.html',{'form':form})
+    else:
+        form = Signup(request.POST)
+        return render(request,'signup.html',{'form':form})
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+def home(request):
+    return render(request, 'home.html')
